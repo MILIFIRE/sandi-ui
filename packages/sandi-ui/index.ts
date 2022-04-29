2ba8789e38b93559d72332f6bd47d56ecda90049
songshuai <songshuai@aiyoutoo.com>
2022-04-29 15:20:30 +0800

    Update AnimationAction AnimationMixer documentation

--- packages/sandi-ui/index.ts
+++ packages/sandi-ui/index.ts
@@ -4,6 +4,7 @@ import components from './component'
 import type { App } from 'vue';
 import v3dCore from '@sandi-ui/core';
 export * from '@sandi-ui/components'
+export * from "@sandi-ui/enum"
 const installer = (app: App) => {
     const v3dCoreInstance = new v3dCore();
     app.config.globalProperties.$getv3dCore = () => v3dCoreInstance;
